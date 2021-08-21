3311638a265d94850d9d2fcc81d7ca3f9957d515
chae1_1111 <dla4212@naver.com>
2021-08-21 14:48:36 +0900

    detail 적용

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,7 +1,7 @@
 import styles from './search_header.module.css';
 import React, { useRef } from 'react';
 
-const SearchHeader = ({ onSearch }) => {
+const SearchHeader = ({ onSearch, onLogoClick }) => {
     const inputRef = useRef();
     const handleSearch = () => {
         const value = inputRef.current.value;
@@ -17,7 +17,7 @@ const SearchHeader = ({ onSearch }) => {
     };
     return (
         <header className={styles.header}>
-            <div className={styles.logo}>
+            <div className={styles.logo} onClick={() => onLogoClick()}>
                 <img className={styles.img} src='./images/logo.png' alt='logo' />
                 <h1 className={styles.title}>MyYoutube</h1>
             </div>
